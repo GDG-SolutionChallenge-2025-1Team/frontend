05d1d00f45134dc190708f68aeca942152fdefbf
indianapoly <hyeonlimgo5@gmail.com>
2025-02-04 19:52:41 +0900

    design: add Color

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  
+  static const Color mainSkyBlue = Color(0xFFC0E7F5);
+  static const Color mainYellow = Color(0xFFFFF4C0); 
+  
+  // Text 색상 정의
+  static const Color textPink = Color(0xFFffa4a5);   
+  static const Color textSkyBlue = Color(0xFFC0E7F5);
+  static const Color textBrown = Color(0xFFb48e68);  
+  static const Color textBlack = Color(0xFF000000);  
+  
+  // 버튼 색상 정의
+  static const Color buttonYellow = Color(0xFFFFB30E);
+  
+  // 상자 색상 정의
+  static const Color boxGrey = Color(0xFFD9D9D9);    
+}
