2ee01f2b493d132a1e2f71e23d14e4737374a945
indianapoly <hyeonlimgo5@gmail.com>
2025-02-04 20:06:37 +0900

    design: asset 추가

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  AppAssets._();
+
+  // 이미지 경로
+  static const String ImageBear = 'assets/images/image_bear.png';
+
+  // 아이콘 경로
+  static const String IconBackButton = 'assets/icons/icon_back_button.png';
+  static const String IconNextButton = 'assets/icons/icon_next_button.png';
+  static const String IconCalendar = 'assets/icons/icon_calendar.png';
+}
