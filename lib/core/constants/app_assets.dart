57836379c5ea2cf291eb2032300ae5b8d1586aa8
unknown <phl2510@naver.com>
2025-03-01 20:46:49 +0900

    android로그인 연결

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -2,10 +2,10 @@ class AppAssets {
   AppAssets._();
 
   // 이미지 경로
-  static const String ImageBear = 'assets/images/image_bear.png';
+  static const String imageBear = 'assets/images/image_bear.png';
 
   // 아이콘 경로
-  static const String IconBackButton = 'assets/icons/icon_back_button.png';
-  static const String IconNextButton = 'assets/icons/icon_next_button.png';
-  static const String IconCalendar = 'assets/icons/icon_calendar.png';
+  static const String iconBackButton = 'assets/icons/icon_back_button.png';
+  static const String iconNextButton = 'assets/icons/icon_next_button.png';
+  static const String iconCalendar = 'assets/icons/icon_calendar.png';
 }
