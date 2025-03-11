66981d43dbba82de28913d9fdc19816e4ab1e43a
unknown <phl2510@naver.com>
2025-03-11 23:45:11 +0900

    홈화면 3가지 기능 기본형태 수정

--- lib/screens/daily_study/daily_study.dart
+++ lib/screens/daily_study/daily_study.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gdg_soogsil_solution_challenge_1team_frontend/core/theme/app_colors.dart';
+import 'package:gdg_soogsil_solution_challenge_1team_frontend/widgets/wave_painter.dart';
 
 class DailyStudyScreen extends StatelessWidget {
   const DailyStudyScreen({super.key});
@@ -6,8 +8,124 @@ class DailyStudyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("오늘의 학습")),
-      body: Center(child: Text("여기에 오늘의 학습 내용을 추가하세요!")),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.mainYellow,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: CustomPaint(
+                painter: WavePainter(
+                  color: AppColors.mainSkyBlue,
+                  isTopWave: true,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 40,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Text(
+                '1일차',
+                style: TextStyle(
+                  fontSize: 65,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textPink,
+                  fontFamily: 'BMJUA',
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 300,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '오늘의 주제!',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'BMJUA',
+                      color: AppColors.textBrown,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '행복',
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'BMJUA',
+                      color: AppColors.textPink,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 20,
+            child: Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.rotationY(3.1416),
+              child: Image.asset(
+                'assets/icons/icon_next_button.png',
+                width: 60,
+                height: 60,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: Image.asset(
+              'assets/icons/icon_next_button.png',
+              width: 60,
+              height: 60,
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Text(
+                '1',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'BMJUA',
+                  color: AppColors.mainSkyBlue,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
